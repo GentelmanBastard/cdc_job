57009f3327427527cf3d3b33d9f08e41ffbec637
Kucman Mateusz <mateusz.kucman@igt.com>
2019-04-15 16:37:55 +0200

    Added radiobuttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,11 @@
 package sample;
 
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
@@ -15,24 +14,30 @@ public class Controller {
     @FXML
     private TextField date;
     @FXML
-    private Button calculate;
+    private RadioButton date_radio;
+    @FXML
+    private RadioButton cdc_radio;
+    @FXML
+    private Button calculate_cdc;
+    @FXML
+    private Button calculate_date;
     @FXML
     private Button clear_input;
     @FXML
-    Button clear_log;
+    private Button clear_log;
     @FXML
     private TextArea log;
 
     public void onButtonClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "Wrong date format. Should be YYYY-MM-DD");
         String date_string = date.getText();
-        if (calculate.isFocused()) {
+        String startDate = "1992-10-01";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate start_date = LocalDate.parse(startDate, formatter);
+        if (calculate_cdc.isFocused() && date_radio.isSelected()) {
             try {
-//                LocalDate localDate = LocalDate.now();
-                String startDate = "1992-10-01";
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
                 LocalDate past_date = LocalDate.parse(date_string, formatter);
-                LocalDate start_date = LocalDate.parse(startDate, formatter);
+//                LocalDate localDate = LocalDate.now();
                 long result = ChronoUnit.DAYS.between(start_date, past_date);
                 log.appendText("CDC: " + result + "\n");
             } catch (DateTimeParseException e) {
@@ -40,7 +45,15 @@ public class Controller {
                 date.clear();
                 date.requestFocus();
             }
-        } else if (clear_input.isFocused()) {
+        } else if (calculate_date.isFocused() && cdc_radio.isFocused()){
+            try{
+                int cdc = Integer.parseInt(date_string);
+                LocalDate result = start_date.plusDays(cdc);
+                System.out.println(result);
+                log.appendText("Date: " + result.toString() + "\n");
+            }catch (Exception e){}
+        }
+        else if (clear_input.isFocused()) {
             date.clear();
         } else if (clear_log.isFocused()) {
             log.clear();
